550fc8e519330fa263ee6778ea2d2b6f8a4db688
Jihee Hong <486480@student.saxion.nl>
2022-08-17 16:50:30 +0200

    card component added

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
+import styles from './card.module.css';
 
-function Card() {
-  return <div>Card</div>;
+function Card({ card }) {
+  return (
+    <div className={styles.card}>
+      <section className={styles.cardDetails}>
+        <h3 className={styles.cardName}>{card.name}'s passport</h3>
+        <img
+          className={styles.image}
+          src={`./images/${card.fileURL}`}
+          alt={card.fileName}
+        />
+      </section>
+      <section className={styles.cardDetails}>
+        <span>Name: {card.name}</span>
+        <span>Born in {card.birthDate}</span>
+        <span>Breed: {card.breed}</span>
+        <span>Owner: {card.owner}</span>
+      </section>
+      <section className={styles.vaccines}>
+        <table>
+          <tr>
+            <th>Vaccine name</th>
+            <th>Injection Date</th>
+          </tr>
+          {card.vaccines.map((vaccine) => (
+            <tr>
+              <th>{vaccine.name}</th>
+              <th>{vaccine.date}</th>
+            </tr>
+          ))}
+        </table>
+      </section>
+      <img className={styles.stamp} src='./images/stamp.png' alt='Stamp' />
+    </div>
+  );
 }
 
 export default Card;
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,11 +1,44 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Footer from '../footer/footer';
 import Header from '../header/header';
 import styles from './main.module.css';
 import { useNavigate } from 'react-router-dom';
 import CardForm from '../cardForm/cardForm';
+import Card from './../card/card';
 
 function Main({ firebaseAuthService }) {
+  const [cards, setCards] = useState([
+    {
+      id: '1',
+      name: 'Coco',
+      birthDate: '2018-03-24',
+      theme: 'blue',
+      breed: 'Bichon frise',
+      fileName: 'coco',
+      fileURL: 'coco.jpeg',
+      owner: 'Tim',
+      vaccines: [
+        { name: 'Canine distemper virus', date: '2018-09-11' },
+        { name: 'Rabies', date: '2019-09-11' },
+        { name: 'Canine parvovirus', date: '2019-12-11' },
+        { name: 'Lyme disease', date: '2020-12-11' },
+      ],
+    },
+    {
+      id: '2',
+      name: 'Song',
+      birthDate: '2019-06-01',
+      theme: 'pink',
+      breed: 'Bichon frise',
+      fileName: 'Song',
+      fileURL: 'coco.jpeg',
+      owner: 'Young',
+      vaccines: [
+        { name: 'Canine distemper virus', date: '2019-09-11' },
+        { name: 'Rabies', date: '2020-09-11' },
+      ],
+    },
+  ]);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -24,9 +57,15 @@ function Main({ firebaseAuthService }) {
       <div className={styles.content}>
         <section className={styles.contentContainer}>
           <h1 className={styles.title}>Editor</h1>
+          {cards.map((card) => (
+            <CardForm card={card} />
+          ))}
         </section>
         <section className={styles.contentContainer}>
           <h1 className={styles.title}>Preview</h1>
+          {cards.map((card) => (
+            <Card card={card} />
+          ))}
         </section>
       </div>
       <Footer />
