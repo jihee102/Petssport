5c2cd96b683f7626df3704b328627f1e1348f895
Jihee Hong <486480@student.saxion.nl>
2022-08-17 15:16:20 +0200

    Editor and preview section adde4d

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -3,6 +3,7 @@ import Footer from '../footer/footer';
 import Header from '../header/header';
 import styles from './main.module.css';
 import { useNavigate } from 'react-router-dom';
+import CardForm from '../cardForm/cardForm';
 
 function Main({ firebaseAuthService }) {
   const navigate = useNavigate();
@@ -20,7 +21,14 @@ function Main({ firebaseAuthService }) {
   return (
     <div className={styles.mainContainer}>
       <Header logout={logout} />
-      <div className={styles.content}>main content</div>
+      <div className={styles.content}>
+        <section className={styles.contentContainer}>
+          <h1 className={styles.title}>Editor</h1>
+        </section>
+        <section className={styles.contentContainer}>
+          <h1 className={styles.title}>Preview</h1>
+        </section>
+      </div>
       <Footer />
     </div>
   );
