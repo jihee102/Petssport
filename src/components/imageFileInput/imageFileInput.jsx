d12b6b9eb8b9891a18f7b61befb30bbe957dea04
Jihee Hong <486480@student.saxion.nl>
2022-09-02 17:19:27 +0200

    Added loading spinner

--- src/components/imageFileInput/imageFileInput.jsx
+++ src/components/imageFileInput/imageFileInput.jsx
@@ -1,8 +1,10 @@
 import { async } from '@firebase/util';
 import React, { useRef } from 'react';
+import { useState } from 'react';
 import styles from './imageFileInput.module.css';
 
 function ImageFileInput({ imageUploader, name, onFileChange }) {
+  const [loading, setLoading] = useState(false);
   const inputRef = useRef();
   const onButtonClick = (event) => {
     event.preventDefault();
@@ -10,7 +12,9 @@ function ImageFileInput({ imageUploader, name, onFileChange }) {
   };
 
   const onChange = async (event) => {
+    setLoading(true);
     const result = await imageUploader.upload(event.target.files[0]);
+    setLoading(false);
     onFileChange(result);
   };
   return (
@@ -23,9 +27,13 @@ function ImageFileInput({ imageUploader, name, onFileChange }) {
         className={styles.input}
         onChange={onChange}
       />
-      <button className={styles.button} onClick={onButtonClick}>
-        {name || 'No File'}
-      </button>
+      {loading ? (
+        <div className={styles.loading}></div>
+      ) : (
+        <button className={styles.button} onClick={onButtonClick}>
+          {name || 'No File'}
+        </button>
+      )}
     </div>
   );
 }
